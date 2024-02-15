fec794fabbcc74b4ed902ed1b9b56efe00401f72
Matthew Vandergrift <mvandergrift@gmail.com>
2024-02-14 17:13:44 -0700

    Initial commit of NATS tests

---
+++ main.go
@@ -0,0 +1,71 @@
+package main
+
+import (
+	"context"
+	"crypto/rand"
+	"fmt"
+
+	"github.com/nats-io/nats.go"
+	"github.com/nats-io/nats.go/jetstream"
+)
+
+func main() {
+
+}
+
+func connectToNats() (*nats.Conn, error) {
+	nc, err := nats.Connect("nats://localhost:4222")
+	if err != nil {
+		return nil, err
+	}
+	return nc, nil
+}
+
+func createKVStream(nc *nats.Conn, store string) (jetstream.KeyValue, error) {
+	js, err := jetstream.New(nc)
+	if err != nil {
+		return nil, err
+	}
+	ctx := context.Background()
+	kv, err := js.CreateKeyValue(ctx, jetstream.KeyValueConfig{
+		Bucket: store,
+		// Description: fmt.Sprintf("Finite Store for %s", store),
+		// TTL:         c.v.GetDuration("NATS.JobResponseKVTTLMinutes") * time.Minute,
+		// MaxBytes:    c.v.GetInt64("NATS.JobResponseKVMaxGB") * 1024 * 1024 * 1024,
+		// Replicas:    c.v.GetInt("NATS.JobResponseKVReplicas"),
+	})
+	if err != nil {
+		return nil, err
+	}
+	return kv, nil
+}
+
+func fillKV(kv jetstream.KeyValue, size int) error {
+	ctx := context.Background()
+
+	value := make([]byte, 1024*1024)
+	rand.Read(value)
+
+	for i := 0; i < size; i++ {
+		key := fmt.Sprintf("key-%d", i)
+		_, err := kv.Put(ctx, key, value)
+		if err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
+func getKVRange(kv jetstream.KeyValue, size int) error {
+	ctx := context.Background()
+
+	for i := 0; i < size; i++ {
+		key := fmt.Sprintf("key-%d", i)
+		_, err := kv.Get(ctx, key)
+		if err != nil {
+			return err
+		}
+	}
+
+	return nil
+}
---
+++ stream_test.go
@@ -0,0 +1,59 @@
+package main
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestConnectToNats(t *testing.T) {
+	_, err := connectToNats()
+	if err != nil {
+		t.Errorf("connectToNats() failed: %v", err)
+	}
+}
+
+func TestCreateKVStream(t *testing.T) {
+	nc, err := connectToNats()
+	assert.NoError(t, err)
+	assert.NotNil(t, nc)
+
+	kv, err := createKVStream(nc, "test")
+	assert.NoError(t, err)
+	assert.NotNil(t, kv)
+}
+
+func TestFillKV(t *testing.T) {
+	nc, err := connectToNats()
+	assert.NoError(t, err)
+	assert.NotNil(t, nc)
+
+	kv, err := createKVStream(nc, "test")
+	assert.NoError(t, err)
+	assert.NotNil(t, kv)
+
+	err = fillKV(kv, 100000)
+	assert.NoError(t, err)
+}
+
+func TestGetKVRange(t *testing.T) {
+	RANGE := 10000
+
+	nc, err := connectToNats()
+	assert.NoError(t, err)
+	assert.NotNil(t, nc)
+	kv, err := createKVStream(nc, "test")
+
+	assert.NoError(t, err)
+	assert.NotNil(t, kv)
+
+	t.Run("Fill KV", func(t *testing.T) {
+		err = fillKV(kv, RANGE)
+		assert.NoError(t, err)
+	})
+
+	t.Run("Get KV Range", func(t *testing.T) {
+		err = getKVRange(kv, RANGE)
+		assert.NoError(t, err)
+	})
+}
